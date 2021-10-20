c59aa151642c7e47777d6360cf5def2e02ef042d
Victoria Vysotskaya <vvyysotskaya@gmail.com>
2021-10-20 16:10:13 +0300

    init: start museum project

---
+++ museum/js/script.js
@@ -0,0 +1,5 @@
+function ibg() {
+    $.each($(".ibg"), (function (t, a) {
+        $(this).find("img").length > 0 && $(this).css("background-image", 'url("' + $(this).find("img").attr("src") + '")')
+    }))
+}
