c9eca91cd763e62c38b0c3459e1b67dc31e4022a
Victoria Vysotskaya <vvyysotskaya@gmail.com>
2021-10-21 20:42:54 +0300

    fix: change menu behaviour after click on the ancor link

--- museum/js/script.js
+++ museum/js/script.js
@@ -6,9 +6,17 @@ function ibg() {
 let iconMenu = document.querySelector('.header__menu-icon'),
 	menuBody = document.querySelector('.header__menu-body'),
 	welcomeTitle = document.querySelector('.welcome__title-wrapper'),
+	menuLink = document.querySelector('.header__menu-link'),
 	body = document.querySelector('body');
 
 iconMenu.onclick = function() {
+	iconMenu.classList.toggle('_menu-active');
+	menuBody.classList.toggle('_menu-active');
+	welcomeTitle.classList.toggle('_menu-active')
+	body.classList.toggle('_lock');
+}
+
+menuLink.onclick = function() {
 	iconMenu.classList.toggle('_menu-active');
 	menuBody.classList.toggle('_menu-active');
 	welcomeTitle.classList.toggle('_menu-active')
