cbf0987ce9827612d1069817ff376ae540f069d0
Victoria Vysotskaya <vvyysotskaya@gmail.com>
2021-10-22 22:37:13 +0300

    feat: add explore section

--- museum/js/script.js
+++ museum/js/script.js
@@ -44,5 +44,12 @@ swiper.on('slideChangeTransitionStart', function () {
     let child = parent.querySelector('.slide-number').innerHTML;
     document.querySelector('.welcome__slider-current').innerHTML = child;
 });
-
+const slider = document.querySelector(".slider input");
+const img = document.querySelector(".images .img-2");
+const dragLine = document.querySelector(".slider .drag-line");
+slider.oninput = () => {
+    let sliderVal = slider.value;
+    dragLine.style.left = sliderVal + "%";
+    img.style.width = sliderVal + "%";
+}
 
