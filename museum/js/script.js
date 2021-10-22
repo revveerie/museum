f8b0de60495a0381a3751cc090c1aaa0ca1f8db6
Victoria Vysotskaya <vvyysotskaya@gmail.com>
2021-10-23 00:37:54 +0300

    fix: fix explore slider display

--- museum/js/script.js
+++ museum/js/script.js
@@ -44,12 +44,47 @@ swiper.on('slideChangeTransitionStart', function () {
     let child = parent.querySelector('.slide-number').innerHTML;
     document.querySelector('.welcome__slider-current').innerHTML = child;
 });
-const slider = document.querySelector(".slider input");
-const img = document.querySelector(".images .img-2");
-const dragLine = document.querySelector(".slider .drag-line");
-slider.oninput = () => {
-    let sliderVal = slider.value;
-    dragLine.style.left = sliderVal + "%";
-    img.style.width = sliderVal + "%";
+const slider = document.querySelector("#image-comparison-slider");
+const sliderImgWrapper = document.querySelector("#image-comparison-slider .img-wrapper");
+const sliderHandle = document.querySelector("#image-comparison-slider .handle");
+
+slider.addEventListener("mousemove", sliderMouseMove);
+slider.addEventListener("touchmove", sliderMouseMove);
+
+function sliderMouseMove(event) {
+
+  if(isSliderLocked) return;
+
+  const sliderLeftX = slider.offsetLeft;
+  const sliderWidth = slider.clientWidth;
+  const sliderHandleWidth = sliderHandle.clientWidth;
+
+  let mouseX = (event.clientX || event.touches[0].clientX) - sliderLeftX;
+  if(mouseX < 0) mouseX = 0;
+  else if(mouseX > sliderWidth) mouseX = sliderWidth;
+
+  sliderImgWrapper.style.width = `${((1 - mouseX/sliderWidth) * 100).toFixed(4)}%`;
+  sliderHandle.style.left = `calc(${((mouseX/sliderWidth) * 100).toFixed(4)}% - ${sliderHandleWidth/2}px)`;
+}
+
+let isSliderLocked = false;
+
+slider.addEventListener("mousedown", sliderMouseDown);
+slider.addEventListener("touchstart", sliderMouseDown);
+slider.addEventListener("mouseup", sliderMouseUp);
+slider.addEventListener("touchend", sliderMouseUp);
+slider.addEventListener("mouseleave", sliderMouseLeave);
+
+function sliderMouseDown(event) {
+  if(isSliderLocked) isSliderLocked = false;
+  sliderMouseMove(event);
+}
+
+function sliderMouseUp() {
+  if(!isSliderLocked) isSliderLocked = true;
+}
+
+function sliderMouseLeave() {
+  if(isSliderLocked) isSliderLocked = false;
 }
 
