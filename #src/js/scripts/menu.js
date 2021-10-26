7f7dedabcd207a66fc91805b4b66aef50a5872e1
revveerie <revveerie@gmail.com>
2021-10-26 22:54:34 +0300

    fix: fix menu ancors on tablets and small desktops

--- #src/js/scripts/menu.js
+++ #src/js/scripts/menu.js
@@ -2,7 +2,7 @@ if (document.documentElement.clientWidth <= 1024) {
 	let iconMenu = document.querySelector('.header__menu-icon'),
 	menuBody = document.querySelector('.header__menu-body'),
 	welcomeTitle = document.querySelector('.welcome__title-wrapper'),
-	menuLink = document.querySelector('.header__menu-link'),
+	menuLink = document.querySelectorAll('.header__menu-link'),
 	body = document.querySelector('body');
 
 	iconMenu.onclick = function() {
@@ -12,10 +12,13 @@ if (document.documentElement.clientWidth <= 1024) {
 		body.classList.toggle('_lock');
 	}
 
-	menuLink.onclick = function() {
-		iconMenu.classList.toggle('_menu-active');
-		menuBody.classList.toggle('_menu-active');
-		welcomeTitle.classList.toggle('_menu-active')
-		body.classList.toggle('_lock');
-	}
+	menuLink.forEach(function(item) {
+		item.addEventListener("click", function() {
+			iconMenu.classList.toggle('_menu-active');
+			menuBody.classList.toggle('_menu-active');
+			welcomeTitle.classList.toggle('_menu-active')
+			body.classList.toggle('_lock');
+		})
+	});
 }
+
